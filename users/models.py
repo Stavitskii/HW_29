e8b4aa9609d9cec096438879e03d17d8197b1517
Stavitskii <pochta100@mail.ru>
2022-10-31 18:36:06 +0300

    Probably user db is fixed

--- users/models.py
+++ users/models.py
@@ -31,7 +31,7 @@ class User(models.Model):
     username = models.CharField(max_length=20, unique=True)
     password = models.CharField(max_length=30)
     role = models.CharField(choices=UserRoles.choices, default='member', max_length=60)
-    age = models.PositiveIntegerField()
+    age = models.PositiveIntegerField(null=True)
     location = models.ManyToManyField(Location)
 
     class Meta:
